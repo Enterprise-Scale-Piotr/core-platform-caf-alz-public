4e9037eefba374b15105e0f4600b3c78f8568597
“StanleyXie-Otto“ <stanley.xie@ottogroup.com>
2024-10-31 13:34:48 +0100

    update variables get config from provider azurerm

--- connectivity/main.tf
+++ connectivity/main.tf
@@ -15,12 +15,11 @@ terraform {
   #  }
   backend "azurerm" {
     tenant_id            = "e061f8b7-caf3-45f2-a8bc-491cea9e64aa"
-    subscription_id      = "004b5dd3-9bf5-49d6-9236-f01d6b533dea"
+    subscription_id      = "004b5dd3-9bf5-49d6-9236-f01d6b533dea" #management subscription id for remote tfstate blob
     resource_group_name  = "rg-platform-tfstate"
     storage_account_name = "staplatformtfstate"
     container_name       = "platform-tfstate"
     key                  = "connectivity.tfstate"
-    # access_key is provided via environment variable ARM_ACCESS_KEY
   }
 }
 
@@ -32,7 +31,8 @@ provider "azurerm" {
 }
 
 # Get the current client configuration from the AzureRM provider
-#data "azurerm_client_config" "current" {}
+
+data "azurerm_client_config" "current" {}
 
 # Declare the Azure landing zones Terraform module
 # and provide the connectivity configuration
@@ -52,10 +52,11 @@ module "alz" {
   }
 
   # Base module configuration settings
-  #root_parent_id   = data.azurerm_client_config.current.tenant_id
+  root_parent_id = data.azurerm_client_config.current.tenant_id
   #root_id          = var.root_id
-  root_parent_id   = "e061f8b7-caf3-45f2-a8bc-491cea9e64aa"
-  root_id          = var.root_id
+  #root_parent_id   = "e061f8b7-caf3-45f2-a8bc-491cea9e64aa"
+  root_id = var.root_id
+  #root_name        = var.root_name
   default_location = "westeurope"
 
   # Disable creation of the core management group hierarchy
